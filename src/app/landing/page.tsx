8a82619a09bba7c24ad53089e0455e83e1addb24
Lyka <bin.lyka@institute.pse.ngo>
2025-06-30 14:54:16 +0700

    first commit

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,10 @@
+const Landing = () => {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page</h1>
+      <p>This is the main content of the landing page.</p>
+    </div>
+  );
+};
+
+export default Landing;
